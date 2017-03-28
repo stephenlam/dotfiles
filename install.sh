23bc79602f3e11a37e174ce7fcdc2ad6a10af718
Stephen Lam <stephenzlam@gmail.com>
2017-03-28 11:54:43 -0400

    add .gitconfig

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Create symlinks
 ln -sfv "$DOTFILES_DIR/zsh/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
