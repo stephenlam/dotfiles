67f1dd1fa77e89c39385cd79de9c1eb677806410
Stephen Lam <stephenzlam@gmail.com>
2017-03-29 20:39:19 +0000

    create directory before creating symlinks

--- install.sh
+++ install.sh
@@ -12,5 +12,7 @@ ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/xmonad/" ~/.xmonad
 ln -sfv "$DOTFILES_DIR/konsole/konsolerc" ~/.config/konsolerc
+
+mkdir -p ~/.local/share/konsole
 ln -sfv "$DOTFILES_DIR/konsole/base16-ateliercave-light.colorscheme" ~/.local/share/konsole/base16-ateliercave-light.colorscheme
 ln -sfv "$DOTFILES_DIR/konsole/CodingCave.profile" ~/.local/share/konsole/CodingCave.profile
